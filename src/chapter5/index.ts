03605102f406a80cdcdfb3f33ad24b1ae4ce6cc8
caesiumy <dbs2636@gmail.com>
2021-07-22 00:22:43 +0900

    chapter5: Add chess class

--- src/chapter5/index.ts
+++ src/chapter5/index.ts
@@ -1,10 +1,55 @@
-class Game {}
+type Color = "Black" | "White";
+type FileColumn = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
+type RankRow = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
-class Piece {}
+class Position {
+  constructor(private file: FileColumn, private rank: RankRow) {}
 
-class Position {}
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
+}
 
-class King extends Piece {}
+abstract class Piece {
+  protected position: Position;
+  constructor(private readonly color: Color, file: FileColumn, rank: RankRow) {
+    this.position = new Position(file, rank);
+  }
+
+  moveTo(position: Position) {
+    this.position = position;
+  }
+  abstract canMoveTo(position: Position): boolean;
+}
+
+class Game {
+  private pieces = Game.makePieces();
+
+  private static makePieces() {
+    return [
+      new King("White", "E", 1),
+      new King("Black", "E", 8),
+
+      new Queen("White", "D", 1),
+      new Queen("Black", "D", 8),
+
+      new Bishop("White", "C", 1),
+      new Bishop("White", "F", 1),
+      new Bishop("Black", "C", 8),
+      new Bishop("Black", "F", 8),
+    ];
+  }
+}
+
+class King extends Piece {
+  canMoveTo(position: Position) {
+    const distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
 class Queen extends Piece {}
 class Bishop extends Piece {}
 class Knight extends Piece {}
